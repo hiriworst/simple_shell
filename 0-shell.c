4341e0b2b5d99535fa929f68ff5432e417b93007
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-26 08:59:57 -0700

    shell

---
+++ 0-shell.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function main
+ * Return: nothing
+ */
+
+int main()
+{
+	printf("anything should check\n");
+	return(0);
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main();
+
+#endif
