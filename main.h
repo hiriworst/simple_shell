acf6b1f7d1d5c33efcd9009dbee01133186a9ab3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-28 02:19:13 -0700

    add

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SHELL_H
+#define SHELL_H
 
+/*LIBRARIES*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,18 +9,20 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-int _strcmp(char *str_len, char *str_ptr);
-char *_strcpy(char *ptr_where_to, char *ptr_len);
-char *get_path_location(char *command);
-void *_calloc(unsigned int input_arr, unsigned int size);
-void _var_location(void);
-int run_func(char **args);
-int line_free(char *buff);
-char **_split(char *str, char *take_out);
-char *_strcat(char *ptr_where_to, char *ptr_len);
-int _strlen(char *str);
-char *_capture(char *ptr_var);
+/*PROTOTYPES*/
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+char *search_path(char *command);
+char **_split(char *str, char *sep);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *_getenv(char *env_var);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void _env(void);
+int execute(char **args);
+int empty_line(char *buff);
 
+/* GLOBAL VAR*/
 extern char **environ;
 
 #endif
