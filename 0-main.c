17b0ccc542e710bccceb4adcae695280247030ed
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-28 00:24:56 -0700

    commit

--- 0-main.c
+++ 0-main.c
@@ -1,43 +1,50 @@
 #include "main.h"
-#include <stdlib.h>
-
 
 /**
- * main - main function to run
- * @argc: args count
- * @argv: vector
- * Return: 0 on success
+ * main - open shell, project base
+ * Return: int
  */
-int main(int argc, char **argv)
-{
-	    char *command = NULL;
-	    char **command_now = NULL;
-	    
-	    config data;
-	(void)argc;
-	signal(SIGINT, init_manage);
-	initialize(&data);
-	data.shell_name = argv[0];
-	main_unix(&data);
-	return (0);
-}
 
-/**
- * initialize - initialize member values
- * @data: input data
- * Return: on success
- */
-config *initialize(config *data)
+int main(void)
 {
-	data->env = generateLinkedList(environ);
-	data->env_list = NULL;
-	data->args = NULL;
-	data->buffer = NULL;
-	data->path = _getenv("PATH", environ);
-	data->full_path = NULL;
-	data->count_line = 0;
-	data->shell_name = NULL;
-	data->error_status = 0;
-	return (data);
-}
+	char *buff = NULL, **args;
+	size_t read_size = 0;
+	ssize_t buff_size = 0;
+	int exit_status = 0;
 
+	while (1)
+	{
+		if (isatty(0))
+			printf("hsh$ ");
+
+		buff_size = getline(&buff, &read_size, stdin);
+		if (buff_size == -1 || _strcmp("exit\n", buff) == 0)
+		{
+			free(buff);
+			break;
+		}
+		buff[buff_size - 1] = '\0';
+
+		if (_strcmp("env", buff) == 0)
+		{
+			_env();
+			continue;
+		}
+
+		if (empty_line(buff) == 1)
+		{
+			exit_status = 0;
+			continue;
+		}
+
+		args = _split(buff, " ");
+		args[0] = search_path(args[0]);
+
+		if (args[0] != NULL)
+			exit_status = execute(args);
+		else
+			perror("Error");
+		free(args);
+	}
+	return (exit_status);
+}
