e0e7c4b6cc48c73ef1727026d56cc126254a436f
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-28 18:45:37 +0300

    Update main.h

--- main.h
+++ main.h
@@ -11,16 +11,18 @@
 #include <string.h>
 #include <sys/wait.h>
 
-int exefork(char **bftoken, int goesin, char **environ);
-int _putchar(char c);
-char **linetoken(char *linea, char **env);
-char **findpath(char **environ);
-char **usepath(char *fpat);
-int _stat(char **bftoken, char **pat);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 void find_env(char **environ);
 void _puts(char *str);
 int _strcmp(char *s1, char *s2);
 void *_calloc(unsigned int nmemb, unsigned int size);
+
+int exefork(char **bftoken, int goesin, char **environ);
+int _putchar(char c);
+char **linetoken(char *linea, char **env);
+char **findpath(char **environ);
+char **usepath(char *fpat);
+int _stat(char **bftoken, char **pat);
+
 #endif /* MAIN_H */
