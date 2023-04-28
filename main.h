9d386f14fe6f6a39de0e5f79336db89e970b4773
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-28 19:38:55 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,8 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#define SUPER_USER 2080
 
-#define Space_locate 1024
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,17 +11,6 @@
 #include <string.h>
 #include <sys/wait.h>
 
-char *str_concat(char *s1, char *s2);
-char *_strdup(char *str);
-void find_env(char **environ);
-void _puts(char *str);
-int _strcmp(char *s1, char *s2);
-void *_calloc(unsigned int nmemb, unsigned int size);
-
-void *_my_func_works(unsigned int new_num, unsigned int what_to_ptr);
-
-void *_number_func(unsigned int token, unsigned int lenth_of_token);
-
 
 int exefork(char **bftoken, int goesin, char **environ);
 int _putchar(char c);
@@ -31,5 +18,12 @@ char **linetoken(char *linea, char **env);
 char **findpath(char **environ);
 char **usepath(char *fpat);
 int _stat(char **bftoken, char **pat);
+
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+void find_env(char **environ);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /* MAIN_H */
