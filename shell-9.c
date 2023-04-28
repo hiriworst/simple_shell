d051caf5f6f45eba9b5fd83ceb70fd8deb41bdc7
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-28 18:35:38 +0300

    Update shell-9.c

--- shell-9.c
+++ shell-9.c
@@ -1,37 +1,39 @@
 #include "main.h"
 
 /**
- * linetoken - Breaks the line from prompt into tokens
- * @linea: line from getline
- * @env: env variable
+ * linetoken - separates every lines to different tkkns
+ * @ptr_path_location: first_argument
+ * @place_var: second_argument
  *
- * Return: buffer which is an array of pointers
+ * Return: arr of ptrs
  **/
-char **linetoken(char *linea, char **env)
+char **linetoken(char *ptr_path_location, char **place_var)
 {
-	int count = 0;
-	char *line;
-	char **buffer = _calloc(sizeof(char *), 64);
+	int result = 0;
+	char *path_dir;
+	char **val_buf_variable = _calloc(sizeof(char *), 64);
 
-	line =	strtok(linea, " \n\t\r\a");
+	path_dir =	strtok(ptr_path_location, " \n\t\r\a");
 
-	while (line != NULL)
+	while (path_dir != NULL)
 	{
-		buffer[count] = line;
-		count++;
-		line = strtok(NULL, " \n\t\r\a");
+		val_buf_variable[result] = path_dir;
+		result++;
+		path_dir = strtok(NULL, " \n\t\r\a");
 	}
-	if (buffer[0] == NULL)
-		buffer[count] = "\n";
-
-	if ((_strcmp(buffer[0], "exit") == 0) && buffer[1] == NULL)
+	if (val_buf_variable[0] == NULL)
 	{
-		free(linea);
-		free(buffer);
+		val_buf_variable[result] = "\n";
+	}
+
+	if ((_strcmp(val_buf_variable[0], "exit") == 0) && val_buf_variable[1] == NULL)
+	{
+		free(ptr_path_location);
+		free(val_buf_variable);
 		exit(0);
 	}
-	if ((_strcmp(buffer[0], "env") == 0) && buffer[1] == NULL)
-		find_env(env);
+	if ((_strcmp(val_buf_variable[0], "place_var") == 0) && val_buf_variable[1] == NULL)
+		find_env(place_var);
 
-	return (buffer);
+	return (val_buf_variable);
 }
